3a5964f77f1cd1886c86818778f854be9ae428f1
Tejas <nimasetejas3525@gmail.com>
2021-03-30 22:50:24 +0530

    add cross origin

--- src/main/java/com/veronica/vsm/controller/UserController.java
+++ src/main/java/com/veronica/vsm/controller/UserController.java
@@ -3,6 +3,7 @@ package com.veronica.vsm.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.veronica.vsm.modal.User;
 import com.veronica.vsm.service.UserService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/user")
 public class UserController {
 
